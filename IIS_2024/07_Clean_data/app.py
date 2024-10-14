f428082ef4edb0b00dfa8c01023623f519640a65
Fabricio Solis <sfabricito@gmail.com>
2024-10-13 22:21:14 -0600

    feat: select certain amount of fields of the original CSV file randomly

--- IIS_2024/07_Clean_data/app.py
+++ IIS_2024/07_Clean_data/app.py
@@ -4,6 +4,6 @@ from utils.read_file import read_file
 
 
 if __name__=="__main__":
-    clean_csv('./data/globalterrorismdb.csv','./data/globalTerrorism.csv.csv')
-    convert_csv_parquet('./data/globalTerrorism.csv.csv', './data/globalTerrorism.csv.parquet')
-    read_file('./data/globalTerrorism.csv.parquet')
+    clean_csv('./data/globalterrorismdb.csv','./data/globalTerrorism.csv', 32000)
+    convert_csv_parquet('./data/globalTerrorism.csv', './data/globalTerrorism20000.parquet')
+    read_file('./data/globalTerrorism20000.parquet')
