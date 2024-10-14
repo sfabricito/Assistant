b7490cc07b74b3cd3ea2224b4ae66effaf07a200
Fabricio Solis <sfabricito@gmail.com>
2024-10-13 23:23:15 -0600

    refactor: clean functions

--- IIS_2024/08_Generate_embeddings/utils/generate_embeddings.py
+++ IIS_2024/08_Generate_embeddings/utils/generate_embeddings.py
@@ -2,13 +2,15 @@ from sentence_transformers import SentenceTransformer
 from .system.logger import logger
 
 log = logger()
-
 model = SentenceTransformer('all-mpnet-base-v2')
 
 def generate_embeddings(text):
     try:
-        log.info("Embedding generated succesfully")
-        embedding = model.encode(text).tolist()
+        if isinstance(text, str) and text != '':
+            embedding = model.encode(text).tolist()
+            log.info("Embedding generated succesfully")
+        else:
+            embedding = model.encode('This embedding is not related with anything').tolist()
         return embedding
     except Exception as e:
         log.error(f'An error has occur while generating an embedding. {e}')
